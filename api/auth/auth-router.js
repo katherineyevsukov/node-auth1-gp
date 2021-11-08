44a944da4e80de83eb39ab9f14278c04e9522f61
Kat <kyevsukov@gmail.com>
2021-11-08 16:52:58 -0500

    added hashing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const { add } = require('../users/users-model')
 
 const validatePayload = (req, res, next) => { next() }
 
 router.post('/register', validatePayload, async (req, res, next) => {
     try {
-
+        const { username, password } = req.body
+        const hash = bcrypt.hashSync(password, 8) // 2^8//doesn't need to be super fast
+        const user = { username, password: hash }
+        const createdUser = await add(user)
+        console.log(createdUser)
+        res.status(201).json(createdUser)
     } catch(err){
         next(err)
     }
