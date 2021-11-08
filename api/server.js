eea6e1d6af9676c19ebdc42660a66726bce19bf5
Kat <kyevsukov@gmail.com>
2021-11-08 17:45:05 -0500

    configured cookie

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const usersRouter = require('./users/users-router.js');
 const authRouter = require('./auth/auth-router');
@@ -10,6 +11,18 @@ const server = express();
 server.use(express.static(path.join(__dirname, '../client')));
 server.use(helmet());
 server.use(express.json());
+server.use(session({
+  name: 'monkey', //the name of the session id
+  secret: 'make it long and random', //the session id is actually encrypted
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // in prod, it should be true (only over HTTPS)
+    httpOnly: true, // (the javascript cannot read the cookie)
+  },
+  rolling: true, //push back the expiration date of cookie
+  resave: false, // needed for some session storage solutions
+  saveUninitialized: false // if false, sessions are not stored "by default"
+}))
 
 server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
